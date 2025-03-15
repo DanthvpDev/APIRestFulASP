b4d3beba4b592b0c3e116bc4c1328deee0df9f3c
Daniel Hernández <danthdev02@gmail.com>
2025-03-14 20:46:02 -0600

    Creación de ProveedoresController

--- BackEnd/ProductosApi - copia/ProductosApi/obj/Debug/net8.0/ProductosApi.AssemblyInfo.cs
+++ BackEnd/ProductosApi - copia/ProductosApi/obj/Debug/net8.0/ProductosApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5623ba248a90dbaf92bee81f59472ba565f091")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c99292ca021052a1bdc829cb0719a33b4af01c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
